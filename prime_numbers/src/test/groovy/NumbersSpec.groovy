765916903e2ea1531f3415575171eb6d97a9fddb
Michael Robinson <seraph890@gmail.com>
2016-12-15 17:59:01 +0900

    added extra expects to spec

--- prime_numbers/src/test/groovy/NumbersSpec.groovy
+++ prime_numbers/src/test/groovy/NumbersSpec.groovy
@@ -1,21 +1,19 @@
 import spock.lang.*
 import com.michaelr.Numbers
 
-class IsPrimeSpec extends Specification {
+class NumbersSpec extends Specification {
+
+  @Shared number = new Numbers()
 
   def "should return true for prime number"() {
-    setup:
-    def prime = new Numbers()
-
     expect:
-    prime.isPrime(53) == true
+    number.isPrime(53) == true
+    number.isPrime(2) == true
   }
 
   def "should return false for non-prime number"() {
-    setup:
-    def prime = new Numbers()
-
     expect:
-    prime.isPrime(50) == false
+    number.isPrime(50) == false
+    number.isPrime(1) == false
   }
 }

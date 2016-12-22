b4de01cec751e1332e29ddcafd75cd52c8e131d5
Michael Robinson <seraph890@gmail.com>
2016-12-22 11:04:54 +0900

    fixed up tests, and code so that it actually runs at a decent speed

--- prime_numbers/src/main/groovy/com/michaelr/Numbers.groovy
+++ prime_numbers/src/main/groovy/com/michaelr/Numbers.groovy
@@ -1,16 +1,29 @@
 package com.michaelr
 
 class Numbers {
-  boolean isPrime(int number) {
+  boolean isPrime(double number) {
     boolean prime = true
 
-    (2..number).each {
-      if (number == 1 || number % it == 0 && number != it) {
+    if (number < 2 || number % 2 == 0 && number != 2) {
+      prime = false
+      return prime
+    }
+
+    for(def i = 3; i <= number; i++) {
+      if (i > Math.sqrt(number)) {
+        return prime
+      }
+
+      if (number % i == 0 && number != i) {
         prime = false
-        return true
+        return prime
       }
     }
 
     return prime
+  }
+
+  def generatePrime(int number) {
+    return Math.pow(2, number) - 1
   }
 }
--- prime_numbers/src/test/groovy/NumbersSpec.groovy
+++ prime_numbers/src/test/groovy/NumbersSpec.groovy
@@ -1,4 +1,5 @@
 import spock.lang.*
+import groovy.time.*
 import com.michaelr.Numbers
 
 class NumbersSpec extends Specification {
@@ -7,13 +8,31 @@ class NumbersSpec extends Specification {
 
   def "should return true for prime number"() {
     expect:
-    number.isPrime(53) == true
-    number.isPrime(2) == true
+    number.isPrime(53)
+    number.isPrime(2)
   }
 
   def "should return false for non-prime number"() {
     expect:
-    number.isPrime(50) == false
-    number.isPrime(1) == false
+    !number.isPrime(50)
+    !number.isPrime(1)
+  }
+
+  def "should return basic simple test cases quicker than example"() {
+    setup:
+    def startTime = new Date()
+    def result1 = number.isPrime(number.generatePrime(17))
+    def result2 = number.isPrime(number.generatePrime(31))
+    def result3 = number.isPrime(number.generatePrime(31) + number.generatePrime(17))
+    def result4 = number.isPrime(number.generatePrime(22))
+    def endTime = new Date()
+    TimeDuration duration = TimeCategory.minus(endTime, startTime)
+
+    expect:
+    result1
+    result2
+    !result3
+    !result4
+    duration.seconds < 15 // runs in 285 miliseconds on my 2012 mbp
   }
 }
